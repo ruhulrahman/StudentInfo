8fd79b7f603ec0bde4190698aa9e09fa1d5cf51f
ruhulrahman <ruhulrahman2233@gmail.com>
2019-10-15 22:10:51 +0600

    first commit

---
+++ app/src/main/java/com/example/studentinfo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.studentinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.studentinfo.databinding.ActivityMainBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private String name, department;
+    // Write a message to the database
+    private DatabaseReference databaseStudent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding =  DataBindingUtil.setContentView(this, R.layout.activity_main);
+        //NO error here now.
+
+        databaseStudent = FirebaseDatabase.getInstance().getReference("student");
+        //Please see here. where is the problem in this line
+        binding.AddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addStudentInfo();
+            }
+        });
+
+    }
+
+    private void addStudentInfo() {
+        name = binding.nameET.getText().toString().trim();
+        department = binding.departmentSn.getSelectedItem().toString().trim();
+
+        if(!TextUtils.isEmpty(name)){
+            String id = databaseStudent.push().getKey();
+            Student student = new Student(id, name, department);
+            databaseStudent.setValue(student);
+            Toast.makeText(this, "Data Save Successfully", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Name field is empty", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+//apps crahsed
---
+++ app/src/main/java/com/example/studentinfo/Student.java
@@ -0,0 +1,27 @@
+package com.example.studentinfo;
+
+public class Student {
+    private String id, name, department;
+
+    public Student() {
+
+    }
+
+    public Student(String id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}
