9120b72e472922b7e1ef8fc9fbc1ea83fbd2003a
ruhulrahman <ruhulrahman2233@gmail.com>
2019-10-28 08:12:19 +0600

    style.xml modified

--- app/src/main/java/com/example/studentinfo/model/User.java
+++ app/src/main/java/com/example/studentinfo/model/User.java
@@ -1,26 +1,30 @@
 package com.example.studentinfo.model;
 
 public class User {
-    private String id, email, name, phone;
+    private String id, email, password, name, phone;
 
     public User() {
 
     }
 
-    public User(String id, String email, String name) {
+    public User(String id, String email, String password) {
         this.id = id;
         this.email = email;
+        this.password = password;
+    }
+
+    public User(String id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
         this.name = name;
     }
 
-    public User(String id, String email, String name, String phone) {
+    public User(String id, String email, String password, String name, String phone) {
         this.id = id;
         this.email = email;
+        this.password = password;
         this.name = name;
-        this.phone = phone;
-    }
-
-    public User(String phone) {
         this.phone = phone;
     }
 
@@ -34,6 +38,10 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getName() {
