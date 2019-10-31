f649f122bfd1cf8df5aab448152d9168a01d6033
ruhulrahman <ruhulrahman2233@gmail.com>
2019-10-31 16:43:58 +0600

    user name Textview Added

--- app/src/main/java/com/example/studentinfo/activity/MainActivity.java
+++ app/src/main/java/com/example/studentinfo/activity/MainActivity.java
@@ -17,18 +17,25 @@ import com.example.studentinfo.databinding.ActivityMainBinding;
 import com.example.studentinfo.model.Student;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    private String name, department;
+    private String userName, name, department;
     DatabaseReference databaseStudent;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding =  DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        userName = firebaseAuth.getCurrentUser().getDisplayName();
+        binding.userNameTV.setText(userName);
 
         databaseStudent = FirebaseDatabase.getInstance().getReference("student");
 
